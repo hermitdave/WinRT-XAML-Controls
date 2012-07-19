faf3605cb63b2813035154aa0084a4806a62b253
Hermit Dave <hermitd@hotmail.com>
2012-07-19 23:07:22 +0100

    first commit with autocomplete textbox

---
+++ ControlsTestApp/AutoCompleteDemo.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace ControlsTestApp
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class AutoCompleteDemo : ControlsTestApp.Common.LayoutAwarePage
+    {
+        public AutoCompleteDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        string prepopVal = "Autocomplete, or Word completion, is a feature provided by many web browsers, e-mail programs, search engine interfaces, source code editors, database query tools, word processors, and command line interpreters. Autocomplete is also available for, or already integrated in, general text editors. Autocomplete involves the program predicting a word or phrase that the user wants to type in without the user actually typing it in completely. This feature is effective when it is easy to predict the word being typed based on those already typed, such as when there are a limited number of possible or commonly used words (as is the case with e-mail programs, web browsers, or command line interpreters), or when editing text written in a highly-structured, easy-to-predict language (as in source code editors). It can also be very useful in text editors, when the prediction is based on a list of words in one or more languages. For special purposes, like medical or technical texts a word list of terms in that field is used. Many autocomplete programs are also learning new words as the user has written them a few times, and can suggest alternatives based on the habits of the individual user. Autocomplete, or word prediction, speeds up human-computer interactions in environments to which it is well suited.";
+        char[] sepArr = { ' ', '.', ',' };
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<string> col = new List<string>();
+            string[] parts = prepopVal.ToLower().Split(sepArr, StringSplitOptions.RemoveEmptyEntries);
+
+            col.AddRange(parts.Distinct());
+
+            this.actbTest.ItemsSource = col;
+
+            base.OnNavigatedTo(e);
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        private async void btnPrompt_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog md = new MessageDialog(this.actbTest.Text, "Test");
+            await md.ShowAsync();
+        }
+    }
+}
---
+++ WinRT-XAML-Controls/AutoCompleteTextBox.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace WinRT_XAML_Controls
+{
+    public sealed class AutoCompleteTextBox : Control
+    {
+        TextBox tb = null;
+        ListBox lb = null;
+        Grid g = null;
+        
+        public AutoCompleteTextBox()
+        {
+            this.DefaultStyleKey = typeof(AutoCompleteTextBox);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.tb = GetTemplateChild("tbChild") as TextBox;
+            this.lb = GetTemplateChild("lbChild") as ListBox;
+
+            this.g = GetTemplateChild("spContainer") as Grid;
+
+            if (tb != null && this.lb != null)
+            {
+                tb.TextChanged += tb_TextChanged;
+            }
+
+            if (this.ItemsSource != null)
+                this.lb.ItemsSource = ItemsSource;
+
+            this.g.MaxHeight = this.MaxHeight;
+        }
+
+        public ICollection<string> ItemsSource
+        {
+            get { return (ICollection<string>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public string Text { get { return (this.tb == null ? null : this.tb.Text); } }
+
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(ICollection<string>), typeof(AutoCompleteTextBox), new PropertyMetadata(null));
+
+        void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.lb.SelectionChanged -= lb_SelectionChanged;
+
+            this.lb.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+            if (String.IsNullOrWhiteSpace(this.tb.Text) || this.ItemsSource == null || this.ItemsSource.Count == 0)
+                return;
+
+            var sel = (from d in this.ItemsSource where d.StartsWith(this.tb.Text) select d);
+
+            if (sel != null && sel.Count() > 0)
+            {
+                this.lb.ItemsSource = sel;
+                this.lb.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                this.lb.SelectionChanged += lb_SelectionChanged;
+            }
+
+            //tb.TextChanged += tb_TextChanged;
+        }
+
+        void lb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.tb.TextChanged -= tb_TextChanged;
+
+            this.tb.Text = (string)this.lb.SelectedValue;
+
+            this.tb.TextChanged += tb_TextChanged;
+
+            this.lb.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+    }
+}
